ee38b77bd8969d749c522c115212d2814e10d3a0
AshleyNex <contact.niyaarli@gmail.com>
2024-12-28 20:24:24 +0700

    Fix pathing issues

--- rename.js
+++ rename.js
@@ -1,11 +1,8 @@
 // Set cover image file path (can be changed dynamically)
-document.getElementById('cover-image').style.backgroundImage = "url('rs.jpg')";
-
-// Set audio file path (can be changed dynamically)
-document.getElementById('audio-player').src = "url('rs.wav')";
+document.getElementById('cover-image').style.backgroundImage = "url('../assets/rs.jpg')";
 
 // Set your song name (It will show below ur cover image)
-document.getElementById('song-name').innerHTML = "Default / Unset";
+document.getElementById('song-name').innerHTML = "Unbroken";
 
 // Set your artist name (It will show below the song name in a smaller font)
-document.getElementById('artist-name').innerHTML = "No Artist Name";
+document.getElementById('artist-name').innerHTML = "Casually Nyx";
